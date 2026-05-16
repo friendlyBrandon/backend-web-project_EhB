60fb0f87eac399aa20e74064dc05b2a6eeb768ba
Brandon <b.willemoons@gmail.com>
2026-05-16 18:01:10 +0200

    Initial GitHub upload + login system with admin mark in db
    Start of GitHub upload, have already imported a login system + db system and added mark is_admin into the users table to mark as admin or not.

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+window._ = _;
+
+import * as Popper from '@popperjs/core';
+window.Popper = Popper;
+
+import 'bootstrap';
+
+import axios from 'axios';
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';   
